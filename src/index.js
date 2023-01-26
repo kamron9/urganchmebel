462bd419d1f56d1a2bf4e43b4d5d4b04c5224378
Kamron <kamronalimov80@gmail.com>
2023-01-27 03:48:31 +0500

    header created

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Routers from './routes'
-import './styles/global.css'
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<Routers />)
